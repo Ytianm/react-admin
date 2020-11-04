eeefbb7822b1fe991aaa51dba13a958c1dcf185c
yetm <yetm@ibumobile.com>
2020-11-04 18:18:21 +0800

    update

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import styles from './index.module.less'
+import { Layout, Menu } from 'antd'
+const { Header } = Layout
 
-const Header = props => {
+const HeaderComp = props => {
     return (
-        <div>header</div>
+        <Header>
+            <div className={styles.headerLeft}>
+                <img className={styles.logoImg} src={require('../assets/imgs/logo-white.png')} alt="zcy" />
+                <div className={styles.title}>智能开放平台</div>
+            </div>
+            <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+                <Menu.Item key="1">
+                    <Link to="/home">首页</Link>
+                </Menu.Item>
+                <Menu.Item key="2">
+                    <Link to="/home">应用分析</Link>
+                </Menu.Item>
+                <Menu.Item key="3">
+                    <Link to="/home">应用中心</Link>
+                </Menu.Item>
+            </Menu>
+        </Header>
     )
 }
 
-export default Header
+export default HeaderComp
